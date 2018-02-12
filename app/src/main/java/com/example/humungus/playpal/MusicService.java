c0e25f999b4ad457dcd3daa7c49924cc021bfdec
mutegi <mutegisamuel0@gmail.com>
2018-02-13 00:34:56 +0300

    added new activity

---
+++ app/src/main/java/com/example/humungus/playpal/MusicService.java
@@ -0,0 +1,8 @@
+package com.example.humungus.playpal;
+
+/**
+ * Created by humungus on 2/13/18.
+ */
+
+public class MusicService {
+}
--- app/src/main/java/com/example/humungus/playpal/activities/MainActivity.java
+++ app/src/main/java/com/example/humungus/playpal/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.humungus.playpal;
+package com.example.humungus.playpal.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
