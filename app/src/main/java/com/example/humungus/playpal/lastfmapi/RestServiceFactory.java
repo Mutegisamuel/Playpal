5cd8c472b99a7d23fef8b09cabdb402b017cc7e4
mutegi <mutegisamuel0@gmail.com>
2018-02-12 23:52:36 +0300

    added the api class and models

---
+++ app/src/main/java/com/example/humungus/playpal/lastfmapi/RestServiceFactory.java
@@ -0,0 +1,8 @@
+package com.example.humungus.playpal.lastfmapi;
+
+/**
+ * Created by humungus on 2/12/18.
+ */
+
+public class RestServiceFactory {
+}
---
+++ app/src/main/java/com/example/humungus/playpal/lastfmapi/callbacks/ArtistInfoListener.java
@@ -0,0 +1,8 @@
+package com.example.humungus.playpal.lastfmapi.callbacks;
+
+/**
+ * Created by humungus on 2/12/18.
+ */
+
+public class ArtistInfoListener {
+}
