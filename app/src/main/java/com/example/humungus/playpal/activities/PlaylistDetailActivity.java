a20296052ac4042dd3d285ace57b23777389fafa
mutegi <mutegisamuel0@gmail.com>
2018-02-13 11:38:58 +0300

    added new Fragments

--- app/src/main/java/com/example/humungus/playpal/activities/PlaylistDetailActivity.java
+++ app/src/main/java/com/example/humungus/playpal/activities/PlaylistDetailActivity.java
@@ -5,11 +5,11 @@ import android.os.Bundle;
 
 import com.example.humungus.playpal.R;
 
-public class NowPlayingActivity extends AppCompatActivity {
+public class PlaylistDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_now_playing);
+        setContentView(R.layout.activity_playlist_detail);
     }
 }
